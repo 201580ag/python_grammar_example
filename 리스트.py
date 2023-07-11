3ce04738a9df414a4641516ddee951519cd23663
201580ag <39054577+201580ag@users.noreply.github.com>
2023-07-11 09:46:52 +0900

    Create 리스트.py

---
+++ 리스트.py
@@ -0,0 +1,12 @@
+# 리스트 생성 및 인덱싱
+numbers = [1, 2, 3, 4, 5]
+print(numbers[2])  # 출력: 3
+
+# 리스트 슬라이싱
+numbers = [1, 2, 3, 4, 5]
+print(numbers[1:4])  # 출력: [2, 3, 4]
+
+# 리스트 반복문
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)  # 출력: apple banana cherry
