51ab6be838bf9fdcc9f7b4a817721dcb5269565d
201580ag <39054577+201580ag@users.noreply.github.com>
2023-07-11 09:45:38 +0900

    Create 변수 및 연산자.py

---
+++ 변수 및 연산자.py
@@ -0,0 +1,11 @@
+# 변수 할당과 연산
+x = 10
+y = 5
+z = x + y
+print(z)  # 출력: 15
+
+# 문자열 연결
+name = "John"
+age = 25
+message = "My name is " + name + " and I am " + str(age) + " years old."
+print(message)  # 출력: My name is John and I am 25 years old.
