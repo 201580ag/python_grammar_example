7c60eeeb003b167447c7fa5e7a295bd4c24846d4
201580ag <39054577+201580ag@users.noreply.github.com>
2023-07-11 10:00:54 +0900

    Create 세트.py

---
+++ 세트.py
@@ -0,0 +1,14 @@
+# 세트 생성
+fruits = {"apple", "banana", "cherry"}
+
+# 세트에 값 추가
+fruits.add("orange")
+
+# 세트에서 값 제거
+fruits.remove("banana")
+
+# 세트 연산
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+union = set1.union(set2)
+intersection = set1.intersection(set2)
