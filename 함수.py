251106628b4da28af2e56ae9e576a0ce0d3ae116
201580ag <39054577+201580ag@users.noreply.github.com>
2023-07-11 09:47:09 +0900

    Create 함수.py

---
+++ 함수.py
@@ -0,0 +1,12 @@
+# 함수 정의와 호출
+def greet(name):
+    print("Hello, " + name + "!")
+
+greet("John")  # 출력: Hello, John!
+
+# 반환값이 있는 함수
+def add(a, b):
+    return a + b
+
+result = add(3, 4)
+print(result)  # 출력: 7
