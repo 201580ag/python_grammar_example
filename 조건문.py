c91e5cc64eae98b6b25b17e8017b3a4425e12851
201580ag <39054577+201580ag@users.noreply.github.com>
2023-07-11 09:46:17 +0900

    Create 조건문.py

---
+++ 조건문.py
@@ -0,0 +1,15 @@
+# if-else 문
+x = 5
+if x > 10:
+    print("x는 10보다 큽니다.")
+else:
+    print("x는 10보다 작거나 같습니다.")
+
+# elif 문
+x = 7
+if x > 10:
+    print("x는 10보다 큽니다.")
+elif x == 10:
+    print("x는 10입니다.")
+else:
+    print("x는 10보다 작습니다.")
